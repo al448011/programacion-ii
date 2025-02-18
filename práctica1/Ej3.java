1351b45c3a27329a6afcac8926a1efb97625af32
alufer8673 <alufer8673@ieselcaminas.org>
2025-02-18 23:54:52 +0100

    2025-02-18

---
+++ práctica1/Ej3.java
@@ -0,0 +1,24 @@
+package práctica1;
+
+import java.util.Scanner;
+
+public class Ej3 {
+    public static void main(String[] args) {
+        System.out.print("Introduce un número: ");
+
+        Scanner entrada = new Scanner (System.in);
+        int n = entrada.nextInt();
+
+        int divisores = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                divisores += 1;
+        }
+
+        if (divisores == 2)
+            System.out.println("Es primo");
+        else
+            System.out.println("No es primo");
+    }
+}
