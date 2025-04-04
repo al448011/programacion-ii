ce913c2fde4d3bb9f0091788b81e9c5a73c0e9fb
al448011 <al448011@uji.es>
2025-04-04 12:15:50 +0200

    exercise Restaurante finished

--- src/practica2/Restaurante.java
+++ src/practica2/Restaurante.java
@@ -1,5 +1,8 @@
 package practica2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Restaurante {
@@ -27,11 +30,29 @@ public class Restaurante {
         return this.posición.distancia(p);
     }
 
-    public static Restaurante[] leeRestaurantes(String nombreFichero){
+    public static Restaurante[] leeRestaurantes(String nombreFichero) throws FileNotFoundException {
 
         Scanner fichero = new Scanner(new File(nombreFichero));
-        longitudVector = fichero.nextInt();
+        int longitudVector = fichero.nextInt();
 
-        Restaurante[] vectorRestaurantes = new vectorRestaurantes[longitudVector];
+        Restaurante[] vectorRestaurantes = new Restaurante[longitudVector];
+
+        for (int i = 0; i < longitudVector; i++){
+            Punto posición = new Punto(fichero.nextInt(),fichero.nextInt());
+            int valoración = fichero.nextInt();
+            String nombre = fichero.next();
+
+            vectorRestaurantes[i] = new Restaurante(nombre, posición, valoración);
+        }
+
+        System.out.println(Arrays.toString(vectorRestaurantes));;
+
+        return vectorRestaurantes;
     }
+
+//    public static void main(String[] args) throws FileNotFoundException {
+//        String rutaFichero = "G:\\Mi unidad\\EI1008 - Programación II\\programacion-ii\\src\\practica2\\ficherosAuxiliares\\restaurantes.txt";
+//        Restaurante[] restaurantes = Restaurante.leeRestaurantes(rutaFichero);
+//    }
+
 }
