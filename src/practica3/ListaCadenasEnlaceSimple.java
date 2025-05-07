769066fd3a42aaa1c1686cae206d81674b7a7ab7
al448011 <al448011@uji.es>
2025-05-07 16:59:44 +0200

    hecho y comprobado add(), add(), clear(), get(), indexOf(), lastIndexOf(), isEmpty()

--- src/practica3/ListaCadenasEnlaceSimple.java
+++ src/practica3/ListaCadenasEnlaceSimple.java
@@ -1,16 +1,18 @@
 package practica3;
 
+import java.util.List;
 import java.util.Objects;
 
 public class ListaCadenasEnlaceSimple implements ListaCadenas {
 
     // Clase anidada
+
     // Nodo con enlace simple
-    private static class Nodo {
+    private static class Nodo{
         String dato;
         Nodo sig;
 
-        Nodo(String dato, Nodo sig) {
+        Nodo(String dato, Nodo sig){
             this.dato = dato;
             this.sig = sig;
         }
@@ -22,13 +24,14 @@ public class ListaCadenasEnlaceSimple implements ListaCadenas {
     // Constructor (por defecto)
 
     // Métodos
-    public boolean add(String s) {
+    public boolean add(String s){
         Nodo nuevo = new Nodo(s, null);
+
         if (primero == null)
             primero = nuevo;
         else{
             Nodo aux = primero;
-            for (int i = 0; i < talla - 1; i++)
+            while (aux.sig != null)
                 aux = aux.sig;
             aux.sig = nuevo;
         }
@@ -36,20 +39,27 @@ public class ListaCadenasEnlaceSimple implements ListaCadenas {
         return true;
     }
 
-    public void add(int i, String s) throws IndexOutOfBoundsException {
+    public void add(int i, String s) throws IndexOutOfBoundsException{
         if (i < 0 || i > size()) throw new IndexOutOfBoundsException();
+
+        Nodo aux = primero;
 
         if (i == talla)
             add(s);
-        else {
-            Nodo aux = primero;
-            for (int j = 0; j < i - 1; j++)
+        else if (i == 0) {
+            primero = new Nodo(s, primero);
+            for (int j = 1; j < talla; j++)
                 aux = aux.sig;
-
-            Nodo nuevo = new Nodo(s,aux);
-
+            talla++;
+        } else {
+            for (int j = 0; j < i; j++)
+                aux = aux.sig;
+            aux = new Nodo(s, aux.sig);
+            for (int j = i; j < talla; j++)
+                aux = aux.sig;
             talla++;
         }
+
     }
 
     public void clear() {
@@ -57,11 +67,12 @@ public class ListaCadenasEnlaceSimple implements ListaCadenas {
         talla = 0;
     }
 
-    public String get(int i) throws IndexOutOfBoundsException {
+    public String get(int i) throws IndexOutOfBoundsException{
         if (i < 0 || i >= size()) throw new IndexOutOfBoundsException();
 
         Nodo aux = primero;
-        for (int j = 0; j != i; j++)
+
+        for (int j = 0; j < i; j++)
             aux = aux.sig;
 
         return aux.dato;
@@ -69,98 +80,140 @@ public class ListaCadenasEnlaceSimple implements ListaCadenas {
 
     public int indexOf(String s) {
         Nodo aux = primero;
-        for (int i = 0; i < talla - 1; i++){
-            if (Objects.equals(s, aux.dato))
-                return i;
+
+        for (int i = 0; i < talla; i++){
+            if (Objects.equals(s, aux.dato)) return i;
+            aux = aux.sig;
         }
+
         return -1;
     }
 
     public int lastIndexOf(String s) {
+
         Nodo aux = primero;
-        int últimaOcurrencia = -1;
+        int ultimaCoincidencia = -1;
 
-        for (int i = 0; i < talla - 1; i++){
-            if (Objects.equals(s, aux.dato))
-                últimaOcurrencia = i;
+        for (int i = 0; i < talla; i++){
+            if (Objects.equals(s, aux.dato)) ultimaCoincidencia = i;
+            aux = aux.sig;
         }
-
-        return últimaOcurrencia;
+        return ultimaCoincidencia;
     }
 
     public boolean isEmpty() {
-        talla = 0;
-        return primero != null;
+        return primero == null;
     }
 
-    public String remove(int i) throws IndexOutOfBoundsException{
-
-        if (i < 0 || i >= size()) throw new IndexOutOfBoundsException();
-
-        Nodo auxAnterior = primero;
-        Nodo auxActual = auxAnterior.sig;
-
-        for (int j = 0; j != i; j++){
-            auxAnterior = auxActual;
-            auxActual = auxActual.sig;
-        }
-
-        auxActual.sig = auxAnterior;
-        return auxActual.dato;
+    public String remove(int i) {
+        return "";
     }
 
     public boolean remove(String s) {
-
-        Nodo auxAnterior = primero;
-        Nodo auxActual = primero.sig;
-
-        for (int i = 1; i < talla - 1; i++){
-            if (Objects.equals(auxAnterior.dato,s)){
-                for (int j = i; i < talla - 1; j++){
-                    auxAnterior = auxActual.sig;
-                }
-                return true;
-            } else {
-                auxAnterior = auxActual;
-                auxActual = auxActual.sig;
-            }
-        }
         return false;
     }
 
-    public String set(int i, String s) throws IndexOutOfBoundsException {
-
-        if (i < 0 || i >= size()) throw new IndexOutOfBoundsException();
-
-        Nodo aux = primero;
-
-        for (int j = 0; j < i; j++){
-            aux = aux.sig;
-        }
-        aux.sig.dato = s;
-        return aux.dato;
+    public String set(int i, String s) {
+        return "";
     }
 
     public int size() {
-
-        Nodo aux = primero;
-        int size = 0;
-
-        while (aux.sig != null)
-            size++;
-
-        return size;
+        return talla;
     }
 
     public String toString(){
 
-        Nodo aux = primero;
-        String string = "[";
+        if (talla == 0) return "[]";
 
-        for (int i = 0; i < talla - 2; i++){
-            string += aux.dato + ", ";
-            aux = primero.sig;
+        Nodo aux = primero;
+        String resultado = "[";
+
+        for (int i = 0; i < talla - 1; i++){
+            resultado += aux.dato + ", ";
+            aux = aux.sig;
         }
-        return string + aux.dato + "]";
+        return resultado + aux.dato + "]";
+    }
+
+    // Programa principal
+
+    public static void main(String[] args) {
+        ListaCadenas lista = new ListaCadenasEnlaceSimple();
+
+        // probando add(String s)
+        System.out.println("Probando método add(String s):");
+        lista.add("uno");
+        lista.add("dos");
+        lista.add("tres");
+        System.out.println(lista);
+        System.out.println();
+
+        // probando add(int i, String s)
+        System.out.println("Probando método add(int i, String s):");
+        lista.add(0, "cero");
+        lista.add(4, "cuatro");
+        // lista.add(6, "seis");
+        System.out.println(lista);
+
+        System.out.println();
+
+        // probando clear()
+        System.out.println("Probando método clear():");
+        lista.clear();
+        System.out.println(lista);
+
+        System.out.println();
+
+        // probando get(int i)
+        lista.add("cero");
+        lista.add("uno");
+        lista.add("dos");
+        lista.add("tres");
+        lista.add("cuatro");
+
+        System.out.println("Probando método get(int i):");
+        System.out.println(lista.get(4));
+        System.out.println(lista.get(0));
+        System.out.println(lista.get(3));
+
+        System.out.println();
+
+        // probando indexOf(String s)
+        System.out.println("Probando método indexOf(String s):");
+        System.out.println(lista.indexOf("cero"));
+        System.out.println(lista.indexOf("dos"));
+        System.out.println(lista.indexOf("cinco"));
+
+        System.out.println();
+
+        // probando lastIndexOf(String s)
+        lista.add("cero");
+        lista.add("uno");
+        lista.add("dos");
+        lista.add("tres");
+        lista.add("cuatro");
+
+        System.out.println("Probando método indexOf(String s):");
+        System.out.println(lista);
+        System.out.println(lista.lastIndexOf("cero"));
+        System.out.println(lista.lastIndexOf("dos"));
+        System.out.println(lista.lastIndexOf("cinco"));
+
+        System.out.println();
+
+        // probando isEmpty()
+
+        System.out.println("Probando método isEmpty():");
+
+        for (int i = 0; i < 2; i++){
+            if (lista.isEmpty()) {
+                System.out.println(lista);
+                System.out.println("La lista está vacía.");
+            } else {
+                System.out.println(lista);
+                System.out.println("La lista NO está vacía.");
+                lista.clear();
+            }
+        }
     }
 }
