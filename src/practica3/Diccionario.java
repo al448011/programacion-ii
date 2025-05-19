ea99f37d34e9c25432d522ccac4d7f703750619c
al448011 <al448011@uji.es>
2025-05-19 12:38:08 +0200

    comentando pruebas clase Diccionario

--- src/practica3/Diccionario.java
+++ src/practica3/Diccionario.java
@@ -103,61 +103,61 @@ public class Diccionario {
         }
     }
 
-    public static void main(String[] args) {
-        Diccionario dic = new Diccionario();
-
-        // probando metodo añadir(String unaCadena, int unaCantidad)
-        dic.añadir("manzana", 2);
-        dic.añadir("pera", 1);
-        dic.añadir("platano", 3);
-
-        dic.añadir("manzana", 4);
-        dic.añadir("cereza", 5);
-
-        System.out.println(dic);
-        System.out.println();
-
-        // probando metodo cadenaConMayorCantidad()
-        System.out.println("La cadena con mayor cantidad es: " + dic.cadenaConMayorCantidad());
-
-        Diccionario dicVacio = new Diccionario();
-        System.out.println("Diccionario vacío: " + dicVacio.cadenaConMayorCantidad());
-
-        Diccionario dicUnico = new Diccionario();
-        dicUnico.añadir("única", 10);
-        System.out.println("Diccionario con un elemento: " + dicUnico.cadenaConMayorCantidad());
-
-        Diccionario dicNegativos = new Diccionario();
-        dicNegativos.añadir("positivo", 5);
-        dicNegativos.añadir("negativo", -3);
-        System.out.println("Diccionario con negativos: " + dicNegativos.cadenaConMayorCantidad());
-
-        Diccionario dicEmpate = new Diccionario();
-        dicEmpate.añadir("primero", 7);
-        dicEmpate.añadir("segundo", 7);
-        dicEmpate.añadir("tercero", 3);
-        System.out.println("Diccionario con empate: " + dicEmpate.cadenaConMayorCantidad());
-
-        System.out.println();
-
-        // probando metodo cantidad(String unaCadena)
-        System.out.println(dic);
-
-        System.out.println("Cantidad de 'manzana': " + dic.cantidad("manzana"));
-        System.out.println("Cantidad de 'cereza': " + dic.cantidad("cereza"));
-        System.out.println("Cantidad de 'pera': " + dic.cantidad("pera"));
-        System.out.println("Cantidad de 'platano': " + dic.cantidad("platano"));
-
-        System.out.println("Cantidad de 'naranja': " + dic.cantidad("naranja"));
-
-        System.out.println("Diccionario vacío, cantidad de 'algo': " + dicVacio.cantidad("algo"));
-
-        System.out.println("Cantidad de cadena vacía: " + dic.cantidad(""));
-
-        dic.añadir("kiwi", 8);
-        System.out.println("Después de añadir 'kiwi', cantidad: " + dic.cantidad("kiwi"));
-
-        dic.añadir("pera", 4);
-        System.out.println("Después de añadir 4 más a 'pera', cantidad: " + dic.cantidad("pera"));
-    }
+//    public static void main(String[] args) {
+//        Diccionario dic = new Diccionario();
+//
+//        // probando metodo añadir(String unaCadena, int unaCantidad)
+//        dic.añadir("manzana", 2);
+//        dic.añadir("pera", 1);
+//        dic.añadir("platano", 3);
+//
+//        dic.añadir("manzana", 4);
+//        dic.añadir("cereza", 5);
+//
+//        System.out.println(dic);
+//        System.out.println();
+//
+//        // probando metodo cadenaConMayorCantidad()
+//        System.out.println("La cadena con mayor cantidad es: " + dic.cadenaConMayorCantidad());
+//
+//        Diccionario dicVacio = new Diccionario();
+//        System.out.println("Diccionario vacío: " + dicVacio.cadenaConMayorCantidad());
+//
+//        Diccionario dicUnico = new Diccionario();
+//        dicUnico.añadir("única", 10);
+//        System.out.println("Diccionario con un elemento: " + dicUnico.cadenaConMayorCantidad());
+//
+//        Diccionario dicNegativos = new Diccionario();
+//        dicNegativos.añadir("positivo", 5);
+//        dicNegativos.añadir("negativo", -3);
+//        System.out.println("Diccionario con negativos: " + dicNegativos.cadenaConMayorCantidad());
+//
+//        Diccionario dicEmpate = new Diccionario();
+//        dicEmpate.añadir("primero", 7);
+//        dicEmpate.añadir("segundo", 7);
+//        dicEmpate.añadir("tercero", 3);
+//        System.out.println("Diccionario con empate: " + dicEmpate.cadenaConMayorCantidad());
+//
+//        System.out.println();
+//
+//        // probando metodo cantidad(String unaCadena)
+//        System.out.println(dic);
+//
+//        System.out.println("Cantidad de 'manzana': " + dic.cantidad("manzana"));
+//        System.out.println("Cantidad de 'cereza': " + dic.cantidad("cereza"));
+//        System.out.println("Cantidad de 'pera': " + dic.cantidad("pera"));
+//        System.out.println("Cantidad de 'platano': " + dic.cantidad("platano"));
+//
+//        System.out.println("Cantidad de 'naranja': " + dic.cantidad("naranja"));
+//
+//        System.out.println("Diccionario vacío, cantidad de 'algo': " + dicVacio.cantidad("algo"));
+//
+//        System.out.println("Cantidad de cadena vacía: " + dic.cantidad(""));
+//
+//        dic.añadir("kiwi", 8);
+//        System.out.println("Después de añadir 'kiwi', cantidad: " + dic.cantidad("kiwi"));
+//
+//        dic.añadir("pera", 4);
+//        System.out.println("Después de añadir 4 más a 'pera', cantidad: " + dic.cantidad("pera"));
+//    }
 }
